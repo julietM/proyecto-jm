ad501ef3136cdf6333b24462580fb10b60c0da82
juliet@21 <julietyartes2@gmail.com>
2024-04-05 17:19:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         soy persona sorda, voy hacer momento 2024 :D
         </p>
         <a
           className="App-link"
